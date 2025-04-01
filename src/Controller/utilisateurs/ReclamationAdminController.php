a7afbc70161b043533e8c5b4dd6b5ef22f501ac9
salimtroudi <salim.troudi@esprit.tn>
2025-04-01 11:02:40 +0100

    entite reclamtion partie client et admin

--- src/Controller/utilisateurs/ReclamationAdminController.php
+++ src/Controller/utilisateurs/ReclamationAdminController.php
@@ -2,15 +2,53 @@
 
 namespace App\Controller\utilisateurs;
 
+use App\Entity\Reclamation;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class ReclamationAdminController extends AbstractController
+#[Route('/admin/reclamations')]
+class ReclamationAdminController extends AbstractController
 {
-    #[Route('/ReclamationsAdmin', name: 'admin_reclamation')]
+    private EntityManagerInterface $entityManager;
+    
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/', name: 'admin_reclamations_index')]
+    #[Route('/', name: 'admin_reclamation')] // Ajoutez cette ligne comme alias
     public function index(): Response
     {
-        return $this->render('utilisateurs/reclamationAdmin.html.twig');
+        $reclamations = $this->entityManager->getRepository(Reclamation::class)
+            ->findBy([], ['dateReclamation' => 'DESC']);
+        
+        return $this->render('utilisateurs/reclamationAdmin.html.twig', [
+            'reclamations' => $reclamations
+        ]);
+    }
+    
+    #[Route('/marquer-traite/{id}', name: 'admin_reclamation_traiter')]
+    public function marquerTraite(Reclamation $reclamation): Response
+    {
+        // Marquer comme traité
+        $reclamation->setEtat('Traité');
+        $this->entityManager->flush();
+        
+        $this->addFlash('success', 'Réclamation #' . $reclamation->getId() . ' marquée comme traitée');
+        return $this->redirectToRoute('admin_reclamation');
+    }
+    
+    #[Route('/details/{id}', name: 'admin_reclamation_details')]
+    public function details(Reclamation $reclamation): Response
+    {
+        // Modifiez cette ligne pour qu'elle pointe vers le bon template
+        return $this->render('utilisateurs/reclamationAdmin.html.twig', [
+            'reclamation' => $reclamation,
+            'showDetails' => true  // Ajoutez ce flag
+        ]);
     }
 }
--- src/Controller/utilisateurs/ReclamationClientController.php
+++ src/Controller/utilisateurs/ReclamationClientController.php
@@ -1,16 +1,57 @@
 <?php
-
+// filepath: c:\Users\asus\TerraNavSymfony1\TerraNavSymfony1\src\Controller\utilisateurs\ReclamationClientController.php
 namespace App\Controller\utilisateurs;
 
+use App\Entity\Reclamation;
+use App\Form\ReclamationClientType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-final class ReclamationClientController extends AbstractController
+class ReclamationClientController extends AbstractController
 {
-    #[Route('/ReclamationsClient', name: 'app_reclamation')]
-    public function index(): Response
+    private EntityManagerInterface $entityManager;
+    
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('utilisateurs/reclamantionClient.html.twig');
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/reclamer', name: 'app_reclamer')]  // Simplified route
+    public function index(Request $request): Response
+    {
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+        
+        // Récupérer les réclamations existantes de l'utilisateur
+        $reclamations = $this->entityManager->getRepository(Reclamation::class)
+            ->findBy(['id_user' => $user], ['dateReclamation' => 'DESC']);
+        
+        // Créer une nouvelle réclamation
+        $reclamation = new Reclamation();
+        $reclamation->setId_user($user);
+        $reclamation->setDateReclamation(new \DateTime());
+        $reclamation->setEtat('Non traité'); // État par défaut
+        
+        $form = $this->createForm(ReclamationClientType::class, $reclamation);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($reclamation);
+            $this->entityManager->flush();
+            
+            $this->addFlash('success', 'Votre réclamation a été envoyée avec succès.');
+            return $this->redirectToRoute('app_reclamer');
+        }
+        
+        return $this->render('utilisateurs/reclamantionClient.html.twig', [
+            'form' => $form->createView(),
+            'reclamations' => $reclamations
+        ]);
     }
 }
