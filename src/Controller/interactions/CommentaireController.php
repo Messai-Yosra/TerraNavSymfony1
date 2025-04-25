85c6198ef8a6d79f95103e1b06b0e38c53baef04
MnariMayar <mayar.mnari@esprit.tn>
2025-04-25 21:31:57 +0100

    mailing professionnelle

--- src/Controller/interactions/CommentaireController.php
+++ src/Controller/interactions/CommentaireController.php
@@ -15,9 +15,9 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class CommentaireController extends AbstractController
-{
-    private $mailer;
+{private $mailer;
     private $urlGenerator;
+    private $entityManager;
 
     public function __construct(EntityManagerInterface $entityManager, MailerInterface $mailer, UrlGeneratorInterface $urlGenerator)
     {
@@ -31,63 +31,84 @@ class CommentaireController extends AbstractController
     {
         // Récupérer le post associé à l'ID
         $post = $entityManager->getRepository(Post::class)->find($postId);
-
+    
         if (!$post) {
             throw $this->createNotFoundException('Le post n\'existe pas');
         }
-
+    
         // Récupérer l'utilisateur connecté
         $user = $this->getUser();
-
+    
         if (!$user) {
             throw $this->createNotFoundException('Utilisateur non connecté');
         }
-
+    
         $commentaire = new Commentaire();
-
-        // Assigner l'utilisateur au commentaire
         $commentaire->setId_user($user);
-
-        // Créer un formulaire pour ajouter un commentaire
+    
+        // Créer le formulaire
         $form = $this->createFormBuilder($commentaire)
-            ->add('contenu', TextareaType::class)
+            ->add('contenu', TextareaType::class, [
+                'label' => 'Votre commentaire',
+                'attr' => [
+                    'placeholder' => 'Écrivez votre commentaire ici...',
+                    'rows' => 5,
+                    'class' => 'form-control'
+                ]
+            ])
             ->getForm();
-
+    
         $form->handleRequest($request);
-
+    
         if ($form->isSubmitted() && $form->isValid()) {
-            // Assigner le post à ce commentaire
+            // Configurer le commentaire
             $commentaire->setId_post($post);
-            // Assigner la date du commentaire
             $commentaire->setDate(new \DateTime());
+            
+            // Mettre à jour le compteur de commentaires
             $post->setNbCommentaires($post->getNbCommentaires() + 1);
-
-            // Sauvegarder dans la base de données
+    
+            // Enregistrement en base de données
             $entityManager->persist($commentaire);
             $entityManager->flush();
-
-            // Envoyer un email au créateur du post
+    
+            // Envoi de l'email de notification
             $postCreator = $post->getId_user();
             if ($postCreator && $postCreator->getEmail()) {
-                $commentLink = 'http://localhost:8000/ChatClient';                
+                $commentLink = $this->urlGenerator->generate('app_chat', [], UrlGeneratorInterface::ABSOLUTE_URL);
+                
+                // Chemin vers le logo dans le dossier public
+                $logoPath = $this->getParameter('kernel.project_dir').'/public/img/TerraNav.png';
+                
+                if (!file_exists($logoPath)) {
+                    throw $this->createNotFoundException('Le logo n\'a pas été trouvé dans public/img/');
+                }
+    
+                // Lire le contenu du logo
+                $logoContent = file_get_contents($logoPath);
+                $logoMime = mime_content_type($logoPath);
+    
                 $email = (new Email())
-                    ->from('mayar.mnari@esprit.tn') // Remplacez par votre adresse d'envoi
-                    ->to($postCreator->getEmail())
-                    ->subject('Nouveau commentaire sur votre post')
-                    ->html("
-                        <h1>Nouveau commentaire</h1>
-                        <p>Un nouveau commentaire a été ajouté à votre post : <strong>{$post->getDescription()}</strong></p>
-                        <p><strong>Commentaire :</strong> {$commentaire->getContenu()}</p>
-                        <p><strong>Publié par :</strong> {$user->getUsername()}</p>
-                        <p><strong>Date :</strong> {$commentaire->getDate()->format('d/m/Y H:i')}</p>
-                        <p><a href='$commentLink'>Voir le commentaire</a></p>
-                    ");
-
+    ->from('terranav4@gmail.com')
+    ->to($postCreator->getEmail())
+    ->subject('Nouveau commentaire sur votre publication')
+    ->embed($logoContent, 'logo', $logoMime)
+    ->html($this->renderView('interactions/new_comment.html.twig', [
+        'postCreator' => $postCreator,
+        'user' => $user,
+        'post' => $post,
+        'commentaire' => $commentaire,
+        'commentLink' => $commentLink
+    ]));
+    
                 $this->mailer->send($email);
             }
+    
+            // Message flash et redirection
+            $this->addFlash('success', 'Votre commentaire a été ajouté avec succès!');
             return $this->redirectToRoute('app_chat');
         }
-
+    
         return $this->render('interactions/newCommentaire.html.twig', [
             'form' => $form->createView(),
             'post' => $post,
@@ -107,7 +128,6 @@ class CommentaireController extends AbstractController
         ]);
     }
     
-    private $entityManager;
 
     #[Route('/comment/{id}/edit', name: 'app_commentaire_edit', methods: ['GET', 'POST'])]
     public function editComment(int $id, Request $request): Response
