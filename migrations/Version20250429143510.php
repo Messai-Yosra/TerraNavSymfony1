c401d4c437c4abafa9f99d88432e6c99ad0be80e
Tirago9 <rayen.khlifi@esprit.tn>
2025-04-29 16:13:23 +0100

    salim megerd 2

--- migrations/Version20250429143510.php
+++ migrations/Version20250429143510.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250426145726 extends AbstractMigration
+final class Version20250429143510 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250426145726 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE utilisateur DROP face_data');
+        $this->addSql('ALTER TABLE utilisateur ADD has_facial_auth TINYINT(1) DEFAULT 0 NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE utilisateur ADD face_data LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE utilisateur DROP has_facial_auth');
     }
 }
